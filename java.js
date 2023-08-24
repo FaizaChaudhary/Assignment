88b204a19c3531591b67d9446d8047299582c9fc
Faiza Chaudhary <faizashafiq117@gmail.com>
2023-08-24 22:23:21 +0500

    Assignment

---
+++ java.js
@@ -0,0 +1,31 @@
+
+// /////  3rd largest Number //////
+
+
+var array = [55,34,65,98,67,32,12,90,56,23];
+array.sort();
+console.log(array);
+console.log(array[array.length-3])
+
+
+// /////  4th largest Number //////
+
+var array = [55,34,65,98,67,32,12,90,56,23];
+array.sort();
+console.log(array);
+console.log(array[array.length-4])
+
+/////  3rd Smallest Number //////
+
+var array = [55,34,65,98,67,32,12,90,56,23];
+array.sort();
+console.log(array)
+console.log(array.sort()[2])   
+
+/////  4th Smallest Number //////
+
+var array = [55,34,65,98,67,32,12,90,56,23];
+array.sort();
+console.log(array)
+console.log(array.sort()[3]);
+
